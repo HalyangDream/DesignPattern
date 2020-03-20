752f2e606053e5c230848ed51422c012873f6e85
taolei <lei.tao@jianyue.in>
2020-03-20 15:33:30 +0800

    测试View.measure相关

--- app/src/main/java/com/example/designpattern/MainActivity.kt
+++ app/src/main/java/com/example/designpattern/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.designpattern
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.designpattern.databinding.ActivityMainBinding
 import com.example.designpattern.factory.FactoryActivity
@@ -15,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var sb = StringBuilder()
+        for (i in 0 until 100) {
+            sb.append("x")
+        }
+        binding.measureTv.text = sb
         setListener()
     }
 
@@ -27,5 +35,36 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(it.context, FactoryActivity::class.java)
             startActivity(intent)
         }
+
+        binding.measureBtn.setOnClickListener {
+            val top = binding.measureLayout.top
+            binding.measureLayout.measure(0, 0)
+            val params = LinearLayout.LayoutParams(
+                binding.measureLayout.measuredWidth,
+                binding.measureLayout.measuredHeight
+            );
+            binding.measureLayout.layoutParams = params
+//            binding.measureLayout.layout(
+//                0, top,
+//                binding.measureLayout.measuredWidth,
+//                binding.measureLayout.measuredHeight + top
+//            )
+//            binding.measureLayout.requestLayout()
+//            binding.rootView.requestLayout()
+            Log.i("Main", "bottom=$top")
+            Log.i("Main", "width=${binding.measureLayout.measuredWidth}")
+            Log.i("Main", "height=${binding.measureLayout.measuredHeight}")
+            startAnim()
+        }
+    }
+
+    private fun startAnim() {
+        val width = binding.measureLayout.measuredWidth
+        val anim =
+            ObjectAnimator.ofFloat(binding.measureLayout, "translationX", 0f, -width.toFloat())
+        anim.duration = 3000
+        anim.repeatCount = -1
+        anim.start()
+
     }
 }
